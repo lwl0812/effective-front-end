0319b3b0114b9aeb88f4c45202ac0276d7942bb4
luwl <luwenli@luwenlideMacBook-Pro-2.local>
2020-07-03 18:10:32 +0800

    feat: 增加 demo code 高亮

---
+++ docs/.vuepress/components/highlight-code.vue
@@ -0,0 +1,21 @@
+<template>
+  <div v-highlightjs>
+    <div :class="[lang]">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HighlightCode',
+  props: {
+    lang: {
+      type: String,
+      default: 'javascript',
+    },
+  },
+};
+</script>
+
+<style></style>
